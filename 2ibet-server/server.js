ef9ba72dc1f49d1f22e8959c91e5bf429ded27c1
jocatins <jocakhamen@gmail.com>
2024-12-08 14:15:18 +0100

    code-refactoring

--- 2ibet-server/server.js
+++ 2ibet-server/server.js
@@ -28,7 +28,7 @@ app.post("/events", (req, res) => {
 	}
 
 	events.push({ eventName, eventDate, bettingOptions });
-	console.log("Current Events:", events);
+	//	console.log("Current Events:", events);
 	res.status(201).json({ message: "Event created successfully." });
 });
 
@@ -39,19 +39,16 @@ app.get("/events", (req, res) => {
 // Endpoint to suspend a betting option
 app.patch("/events/:eventName/options/:optionName/suspend", (req, res) => {
 	const { eventName, optionName } = req.params;
-
-	// Find the event
+	// finding the events
 	const event = events.find((e) => e.eventName === eventName);
 	if (!event) {
 		return res.status(404).json({ error: "Event not found." });
 	}
-
 	// Find the betting option
 	const option = event.bettingOptions.find((o) => o.name === optionName);
 	if (!option) {
 		return res.status(404).json({ error: "Betting option not found." });
 	}
-
 	// Suspend the betting option
 	option.suspended = true;
 	console.log(
@@ -61,12 +58,11 @@ app.patch("/events/:eventName/options/:optionName/suspend", (req, res) => {
 		message: `Betting option '${optionName}' suspended successfully.`,
 	});
 });
-
 // DELETE route (server-side only)
 app.delete("/events", (req, res) => {
 	// Clear the in-memory array
 	events.length = 0;
-	console.log("All events have been deleted.");
+	// console.log("All events have been deleted.");
 	res.json({ message: "All events have been deleted successfully." });
 });
 
